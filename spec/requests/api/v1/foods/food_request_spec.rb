3febda68ab186f58c010c3e1235b43d50e054bd3
JNelson <nelson.jimmy@gmail.com>
2018-07-25 15:49:38 -0600

    added test for showing a single food item

--- spec/requests/api/v1/foods/food_request_spec.rb
+++ spec/requests/api/v1/foods/food_request_spec.rb
@@ -13,4 +13,18 @@ describe 'Foods API' do
     foods = JSON.parse(response.body)
     expect(foods.count).to eq(3)
   end
+
+  it 'can show an individual food' do
+    food1 = Food.create(name: 'Steak', calories: 600)
+    id = food1.id
+
+    get "/api/v1/foods/#{id}"
+
+    expect(response).to be_success
+
+    food = JSON.parse(response.body)
+    expect(food['id']).to eq(id)
+    expect(food['name']).to eq(food1.name)
+    expect(food['calories']).to eq(food1.calories)
+  end
 end
