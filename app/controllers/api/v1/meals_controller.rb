2ea085adceb4f9f4998546ce3c8bd43f3bd4d9da
JNelson <nelson.jimmy@gmail.com>
2018-07-29 12:36:04 -0600

    Successful and failed requests to /api/v1/meals/:id/foods

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::MealsController < ApplicationController
   def index
     render json: Meal.all
   end
+
+  def show
+    if Meal.exists?(params[:id])
+      render json: Meal.find(params[:id])
+    else
+      render status: 404
+    end
+  end
 end
